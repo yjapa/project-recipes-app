d0cf50f6766847bf653970d60218f83f3a95054a
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2021-10-06 15:46:14 -0300

    updates vitor

--- src/components/CardFavorite.jsx
+++ src/components/CardFavorite.jsx
@@ -1,38 +1,65 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useLocation } from 'react-router';
 import shareIcon from '../images/shareIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
+// import FavoritesRecipes from '../pages/FavoritesRecipes';
 
 const CardFavorite = (
   {
+    key,
     indexProps,
+    strID,
+    strType,
     sourceImage,
-    strCategory,
     strRecipeName,
+    strCategory,
+    strArea,
+    strAlcoholicOrNot,
+    fnRemoveFavoriteRecipe,
   },
-) => (
-  <div>
+) => {
+  const location = useLocation();
+
+  const handleClickShareIcon = () => {
+  // executa a função que o joao fez
+    // const fullUrl = window.location.href;
+    const fullURL = location.pathname;
+    console.log(fullURL);
+  };
+
+  return (
     <div>
-      <img
-        alt={ sourceImage }
-        data-testid={ `${indexProps}-horizontal-image` }
-        src={ sourceImage }
-      />
-      <span data-testid={ `${indexProps}-horizontal-top-text` }>{ strCategory }</span>
-      <span data-testid={ `${indexProps}-horizontal-name` }>{ strRecipeName }</span>
-      <img
-        alt="search-icon"
-        data-testid={ `${indexProps}-horizontal-share-btn` }
-        src={ shareIcon }
-      />
-      <img
-        alt="black-heart"
-        // data-testid={ `${indexProps}-horizontal-share-btn` }
-        src={ blackHeartIcon }
-      />
+      <div key={ key }>
+        <img
+          alt={ sourceImage }
+          data-testid={ `${indexProps}-horizontal-image` }
+          src={ sourceImage }
+        />
+        <p data-testid={ `${indexProps}-horizontal-name` }>{ strRecipeName }</p>
+        <p data-testid={ `${indexProps}-horizontal-top-text` }>
+          { strType === 'comida' ? strCategory : strAlcoholicOrNot }
+        </p>
+        { strType === 'comida' ? <p>{ strArea }</p> : null }
+        <button type="button" onClick={ handleClickShareIcon }>
+          <img
+            alt="search-icon"
+            data-testid={ `${indexProps}-horizontal-share-btn` }
+            src={ shareIcon }
+          />
+        </button>
+        <button type="button" onClick={ () => fnRemoveFavoriteRecipe(strID.toString()) }>
+          <img
+            alt="black-heart"
+            // data-testid={ `${indexProps}-horizontal-share-btn` }
+            src={ blackHeartIcon }
+          />
+        </button>
+      </div>
     </div>
-  </div>
-);
+
+  );
+};
 
 CardFavorite.propTypes = {
   indexProps: PropTypes.number,
--- src/pages/FavoritesRecipes.jsx
+++ src/pages/FavoritesRecipes.jsx
@@ -1,27 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
-// import CardFavorite from '../components/CardFavorite';
+import CardFavorite from '../components/CardFavorite';
 
-// Preciso saber de onde virão os dados favoritados sejam eles comidas ou bebidas
+const FavoritesRecipes = () => {
+  // estado do componente que contém o array de receitas favoritas que será renderizado nos cards;
+  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  // função que atualiza o estado conforme o botão "desfavoritar" é acionado;
+  const updateFavoriteRecipesArray = () => {
+    const json = localStorage.getItem('favoriteRecipes');
+    const favoriteRecipesObj = JSON.parse(json);
+    setFavoriteRecipes(favoriteRecipesObj);
+    console.log('favoriteRecipesObj', favoriteRecipesObj);
+  };
 
-const FavoritesRecipes = () => (
-  <div>
-    <Header title="Receitas Favoritas" />
+  const removeFavoriteRecipe = (strIdItem) => {
+    // obtem o array de receitas favoritas no localStorage
+    const arrayStringfyFavRecipes = localStorage.getItem('favoriteRecipes');
+    const arrayObjFavRecipes = JSON.parse(arrayStringfyFavRecipes);
+    // filtra pela exceção
+    console.log('arrayFavRecipes', arrayObjFavRecipes);
+    const favoriteArrayUpdated = arrayObjFavRecipes.filter((item) => (
+      item.id !== strIdItem
+    ));
+    console.log(favoriteArrayUpdated);
+    // atualiza o localStorage
+    localStorage.setItem('favoriteRecipes', favoriteArrayUpdated);
+    // atualiza o estado que está no componente pai para chamar a renderização com o array atualizado
+    setFavoriteRecipes(favoriteArrayUpdated);
+  };
+
+  useEffect(() => (
+    updateFavoriteRecipesArray()
+  ), []);
+
+  return (
     <div>
-      <button data-testid="filter-by-all-btn" type="button">All</button>
-      <button data-testid="filter-by-food-btn" type="button">Food</button>
-      <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+      <Header title="Receitas Favoritas" />
+      <div>
+        <button data-testid="filter-by-all-btn" type="button">All</button>
+        <button data-testid="filter-by-food-btn" type="button">Food</button>
+        <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
+      </div>
+      { favoriteRecipes && favoriteRecipes.map((item, index) => (
+        <CardFavorite
+          Key={ index }
+          indexProps={ index }
+          strID={ item.id }
+          strType={ item.type }
+          sourceImage={ item.image }
+          strRecipeName={ item.name }
+          strCategory={ item.category }
+          strArea={ item.area }
+          strAlcoholicOrNot={ item.alcoholicOrNot }
+          fnRemoveFavoriteRecipe={ removeFavoriteRecipe }
+        />
+      ))}
     </div>
-    {/* { arrayQueContemAListaDeFavoritos.map((item, index) => (
-      <CardFavorite
-        Key={ index }
-        indexProps={ index }
-        sourceImage={ item }
-        strCategory={ item }
-        strRecipeName={ item }
-      />
-    ))} */}
-  </div>
-);
+  );
+};
 
 export default FavoritesRecipes;
